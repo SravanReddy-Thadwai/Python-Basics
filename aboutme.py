9fb023565cf41cf5a7f0bf3a5e488eec5d01fbd1
Sravan Reddy <thesravanreddy@gmail.com>
2026-07-24 11:43:28 +0530

    About Me

---
+++ aboutme.py
@@ -0,0 +1,9 @@
+name = "Sravan Reddy Thadwai"
+age = 19
+college = "RGUKT"
+branch = "CSE"
+print("-------Student Details-------")
+print(f"name    : {name}")
+print(f"Age   : {age}")
+print(f"College : {college}")
+print(f"Branch  : {branch}")
