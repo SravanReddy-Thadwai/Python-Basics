4eb566028ec7e2c87d198ee235dac5113dd24963
Sravan Reddy <thesravanreddy@gmail.com>
2026-07-24 11:56:10 +0530

    Day 1 Basics

---
+++ Day1.py
@@ -0,0 +1,12 @@
+name = "Sravan Reddy"
+age = "20"
+college = "RGUKT"
+print(name)
+print(age)
+print(college)
+#multiple values
+print("name:", name)
+print("age:", age)
+#f strings
+print(f"my name is {name}")
+print(f"iam {age} years old")
