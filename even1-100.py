b4745c5eb5c6a27d08f60038ffad00bfff1bf44b
Sravan Reddy <thesravanreddy@gmail.com>
2026-07-26 12:05:18 +0530

    Printing Numbers Using loops

---
+++ even1-100.py
@@ -0,0 +1,5 @@
+s=0
+for i in range(2,101,2):
+    print(i)
+    s=s+i
+print(f"sum of even is {s}")
---
+++ odd1-100.py
@@ -0,0 +1,5 @@
+s=0
+for i in range(1,100,2):
+    print(i)
+    s=s+i
+print(f"sum of odd is {s}")
